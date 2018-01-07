4c6da595939aef3c2a3e71801b9c1c79a27ecbb2
Yohan Pereira <yohan.pereira@gmail.com>
2018-01-07 20:49:00 +0530

    added code to copy binnaries (with their deps) and fsck

--- gen_initramfs.sh
+++ gen_initramfs.sh
@@ -7,16 +7,28 @@ if [ -z "$DESTINATION" ]; then
     exit 1
 fi
 
+copy_app() {
+    BIN=$1
+    DESTINATION=$2
+    cp -a $BIN $DESTINATION/$BIN
+    #copy deps
+    ldd $BIN | grep -o  -P '\/.*\.[\w:]+' | while read line; do
+	cp $line $DESTINATION/$line
+    done
+}
+
 #make dir structure
-mkdir -p $DESTINATION/{bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys}
+mkdir -p $DESTINATION/{bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys,usr/sbin/,usr/lib64}
 
 #copy required dev files
 sudo cp -a /dev/{null,console,tty} $DESTINATION/dev
 
 #copy apps
-cp -a /bin/busybox $DESTINATION/bin/busybox
+copy_app /bin/busybox $DESTINATION
+copy_app /sbin/fsck $DESTINATION
+copy_app /usr/sbin/fsck.f2fs $DESTINATION
+
 
 #copy init
-
 cp -a init $DESTINATION/init
 chmod +x $DESTINATION/init
